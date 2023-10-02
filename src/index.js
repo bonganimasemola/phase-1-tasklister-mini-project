8cc2b326ce60e301b19f144c7e7435f6a84b90f7
bonganimasemola <bonganimasemola@icloud.com>
2023-10-02 13:49:41 +0300

    phase 1 tasklister almost finished mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.querySelector('form').addEventListener('submit', (e) => {
+   // Prevent default form submission behavior
+  e.preventDefault()
+
+//BLOCKER: console log kept asking me to define description and task, but those words are in form id="", working with code along template but there's a connection i'm not making
+  let task = ''
+  let description = ''
+  handleTasks(e.target.new-task-description.value)
+   })
+  
 });
+
+function handleTasks(tasks) {
+let p = document.createElement('p')
+p.textContent = tasks
+document.querySelector('list').appendChild(p)
+}
+
+
