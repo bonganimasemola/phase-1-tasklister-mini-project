514ca1f3dcb0a6e5e2289fecda5ed9f3ffd57bc7
bonganimasemola <bonganimasemola@icloud.com>
2023-10-03 11:28:59 +0300

    final push

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ document.querySelector('form').addEventListener('submit', (e) => {
    // Prevent default form submission behavior
   e.preventDefault()
 
-//BLOCKER: console log kept asking me to define description and task, but those words are in form id="", working with code along template but there's a connection i'm not making
-  let task = ''
-  let description = ''
-  handleTasks(e.target.new-task-description.value)
+   let task = e.target.querySelector("#new-task-description").value
+   // e.target
+   //console.log(e.target)
+   //document.querySelector("#new-task-description").value
+  //console.log(e.target.querySelector("#new-task-description").value)
+   handleTasks(task);
    })
   
 });
 
-function handleTasks(tasks) {
+
+function handleTasks(task) {
 let p = document.createElement('p')
-p.textContent = tasks
-document.querySelector('list').appendChild(p)
+p.textContent = task
+document.querySelector('#list').appendChild(p)
 }
 
 
